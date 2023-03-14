01c1657d2cd8da934e6a89cb01f1a491aea80f68
ManeeshaPolasa55 <maneeshapolasa55@gmail.com>
2023-03-14 15:24:17 +0530

    quizz

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who developed HTML?",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Jimmy Wales",
+      "Markup",
+      "John",
+      "Tim Berners-Lee"
+    ]
+  },
+    {
+    numb: 2,
+    question: "WhatsApp concurrent model is implemented using _____ programming language.?",
+    answer: "Erlang",
+    options: [
+      "Java",
+      "Node.js",
+      "Erlang",
+      "C++"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is not a programming language?",
+    answer: "Anaconda",
+    options: [
+      "Anaconda",
+      "Python",
+      "TypeScript",
+      "Swift"
+    ]
+  },
+    {
+     numb: 4,
+     question: "Resolving errors in a program is known as...",
+     answer: "Debugging",
+     options: [
+       "Refixing",
+       "Debugging",
+       "Error Checking",
+       "Problem Solving"
+    ]
+   },
+   {
+    numb: 5,
+    question: "Java, Python, PHP, and C++ are examples of ",
+    answer: "High Level Languages",
+    options: [
+      "Low Level Languages",
+      "High Level Languages",
+      "Graphic arts Languages",
+      "Medium level Languages"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Cookies were originally designed for ",
+    answer: "Server side programming",
+    options: [
+      "Client side programming",
+      "Server side programming",
+      "Both client and server side programming",
+      "Socket programming"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is used to declare a constant in PHP?",
+    answer: "Const",
+    options: [
+      "Const",
+      "$Con",
+      "Constant",
+      "#pragma"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Identify the range of byte data types in JavaScript.",
+    answer: "- 128 to 127",
+    options: [
+      "- 128 to 127",
+      "-10 to 9",
+      "-54321 to 54321",
+      "-2147483648 to 2147483647"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the only thing that computers understand?",
+    answer: "Machine Code",
+    options: [
+      "Low Level Languages",
+      "High Level Languages",
+      "Machine Code",
+      "Algorithms"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of these is a programming language?",
+    answer: "Scratch",
+    options: [
+      "Gnaw",
+      "Scratch",
+      "Itch",
+      "Bite"
+    ]
+  },
+];
